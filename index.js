ce39bdbea86105c2367972f42c4c59e959a9aba6
LauffenburgerSergei <lauffenburger@live.fr>
2022-07-04 12:27:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function getColor(){
+
+
+
+const r = Math.floor(Math.random() * 255);
+const g = Math.floor(Math.random() * 255);
+const b = Math.floor(Math.random() * 255);
+
+const color = `rgba(${r}, ${g}, ${b})`;
+
+
+document.body.style.background = color;
+document.body.innerHTML = `<h2>${color}</h2>`
+}
+
+setInterval(getColor, 2000)
